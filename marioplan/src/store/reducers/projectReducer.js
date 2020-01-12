a11b977353ed0f4dc4a96a275168ada407138d6b
aldrin163 <aldrinjenson@gmail.com>
2020-01-12 21:20:49 +0530

    Deleted many redundant files

---
+++ marioplan/src/store/reducers/projectReducer.js
@@ -0,0 +1,13 @@
+const initState = {
+    projects:[
+        {id:'1',title:'Tesla is just an idiot sandwich',content:'blah blah blah'},
+        {id:'1',title:'Yoshi loves Shishimaro',content:'blah blah blah'},
+        {id:'1',title:'Potty break with Yao',content:'blah blah blah'}
+    ]
+}
+
+const projectReducer = (state = initState,action) => {
+    return state
+}
+
+export default projectReducer
--- poketimes/src/reducers/rootReducer.js
+++ poketimes/src/reducers/rootReducer.js
@@ -1,12 +1,20 @@
 const initState = {
     posts:[
-        {id:1,title:"Squirtle's Egg hunt",body:"Lorem ipsum dolor sit, amet consectetur adipisicing elit. Dignissimos tempora, nesciunt officiis rem deserunt modi quae architecto. Dolorem magnam perferendis iusto dolores, labore voluptas similique sed repudiandae nostrum amet odio."},
-        {id:2,title:"Charmander's watery adventure",body:"Lorem ipsum dolor sit, amet consectetur adipisicing elit. Dignissimos tempora, nesciunt officiis rem deserunt modi quae architecto. Dolorem magnam perferendis iusto dolores, labore voluptas similique sed repudiandae nostrum amet odio."},
-        {id:3,title:"Yoshi's sword-play action",body:"Lorem ipsum dolor sit, amet consectetur adipisicing elit. Dignissimos tempora, nesciunt officiis rem deserunt modi quae architecto. Dolorem magnam perferendis iusto dolores, labore voluptas similique sed repudiandae nostrum amet odio."}
+        {id:'1',title:"Squirtle's Egg hunt",body:"Lorem ipsum dolor sit, amet consectetur adipisicing elit. Dignissimos tempora, nesciunt officiis rem deserunt modi quae architecto. Dolorem magnam perferendis iusto dolores, labore voluptas similique sed repudiandae nostrum amet odio."},
+        {id:'2',title:"Charmander's watery adventure",body:"Lorem ipsum dolor sit, amet consectetur adipisicing elit. Dignissimos tempora, nesciunt officiis rem deserunt modi quae architecto. Dolorem magnam perferendis iusto dolores, labore voluptas similique sed repudiandae nostrum amet odio."},
+        {id:'3',title:"Yoshi's sword-play action",body:"Lorem ipsum dolor sit, amet consectetur adipisicing elit. Dignissimos tempora, nesciunt officiis rem deserunt modi quae architecto. Dolorem magnam perferendis iusto dolores, labore voluptas similique sed repudiandae nostrum amet odio."}
     ]
 }
 
 const rootReducer = (state = initState,action) => {
+    if(action.type === 'DELETE_POST'){
+        let newPosts = state.posts.filter(post => post.id !== action.id)
+        return{
+            ...state,
+            posts:newPosts
+        }
+    }
+    
     return state
 }
 
